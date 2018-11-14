eb39213dd0944186ca5a88e1f4d1493829994b10
Pavel Iakovlev <d39reg@gmail.com>
2018-11-15 01:18:45 +0200

    Update modrm.js

--- modrm.js
+++ modrm.js
@@ -1,1 +1,14 @@
 
+function ModRM(byte)
+{
+  var mod = (byte & 0b11000000) >> 6;
+  var reg = (byte & 0b00111000) >> 3;
+  var rm  =  byte & 0b00000111;
+  var sib =  0;
+  
+  if (mod !== 0b11 && rm === 0b100)
+  {
+    sib = getByte();
+  }
+  
+}
